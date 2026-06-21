8b5247d66902049bde3f5987cd986c20aa609ec6
LunahStaryy <advccosta10@gmail.com>
2026-06-21 17:07:46 -0300

    todas as artes necessarias

--- jogo.js
+++ jogo.js
@@ -5,6 +5,7 @@ const menu = document.getElementById('containerMenu');
 const cenario1 = document.getElementById('cenario_1');
 const cenario2 = document.getElementById('cenario_2');
 const cenario3 = document.getElementById('cenario_3');
+const cenario4 = document.getElementById('cenario_4')
 const portaLeft = document.getElementById('portaLEFT')
 const portaRight = document.getElementById('portaRIGHT')
 const portasAbertas = document.getElementById('portasABERTAS')
@@ -13,13 +14,13 @@ const portasFechadas = document.getElementById('portasFECHADAS')
 const dialogosCena1 = document.querySelectorAll('.cena1.text');
 const dialogosCena2 = document.querySelectorAll('.cena2.text');
 const dialogosCena3 = document.querySelectorAll('.cena3.text');
-
+const dialogosCena4 = document.querySelectorAll('.cena4.text')
 const botoes = document.querySelectorAll('.botaoSkipar');
 
 let indiceCena1 = 0;
 let indiceCena2 = 0;
 let indiceCena3 = 0;
-
+let indiceCena4 = 0;
 
 // ESCONDE TODOS OS TEXTOS
 dialogosCena1.forEach(p => {
@@ -31,6 +32,10 @@ dialogosCena2.forEach(p => {
 });
 
 dialogosCena3.forEach(p => {
+    p.style.display = 'none';
+});
+
+dialogosCena4.forEach(p => {
     p.style.display = 'none';
 });
 
@@ -112,5 +117,26 @@ botoes[2].addEventListener('click', function () {
     } else {
         cenario3.style.display = 'none';
 
+        cenario4.style.display = 'block';
+
+        dialogosCena4[0].style.display = 'block';
+    }
+});
+
+
+
+botoes[3].addEventListener('click', function () {
+
+    dialogosCena4[indiceCena4].style.display = 'none';
+
+    indiceCena4++;
+
+    if (indiceCena4 < dialogosCena4.length) {
+
+        dialogosCena4[indiceCena4].style.display = 'block';
+
+    } else {
+
+        cenario4.style.display = 'none';
     }
 });
